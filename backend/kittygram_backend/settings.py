3b3c6df45eb28f2e3ccecb97db1629fa89c63913
Vivorik <viktorvorobev678@gmail.com>
2026-04-21 08:57:50 +1000

    Add GitHub Actions workflow

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -92,6 +92,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -111,3 +112,5 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
+ALLOWED_HOSTS = ['*']
